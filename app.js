e7596c828c19fb8b4451eab4692cdf9566345835
SangeethShiv36 <b.sangeethsivan@gmail.com>
2020-03-04 20:21:25 +0530

    added route and response validation errors

--- app.js
+++ app.js
@@ -4,12 +4,25 @@ require('./models/db');
 const express = require('express');
 const cors = require('cors');
 
+const routeIndex = require('./routes/index.router');
+
 const app = express();
 
 app.use(express.json({ extended: false }));
 
 app.get('/', (req, res) => res.json({ content: 'welcome' }));
 app.use(cors());
+app.use('/api', routeIndex);
+
+app.use((err, req, res, next) => {
+  if (err.name === 'ValidationError') {
+    var valErrors = [];
+    Object.keys(err.errors).forEach(key =>
+      valErrors.push(err.errors[key].message)
+    );
+    res.status(422).send(valErrors);
+  }
+});
 
 app.listen(process.env.PORT, () =>
   console.log(`Server running at port: ${process.env.PORT}`)
