fd92acc1a3eae0624f3ea4c645589a123424a961
SangeethShiv36 <b.sangeethsivan@gmail.com>
2020-03-05 14:05:02 +0530

    added sign in component and made style change in user comp

---
+++ client/src/app/user/sign-in/sign-in.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserService } from 'src/app/shared/user.service';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-sign-in',
+  templateUrl: './sign-in.component.html',
+  styleUrls: ['./sign-in.component.scss']
+})
+export class SignInComponent implements OnInit {
+
+  constructor(public userService: UserService, private router: Router) { }
+
+  model = {
+    email: '',
+    password: ''
+  }
+  emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  serverErrorMessages: string;
+
+  ngOnInit(): void {
+    if (this.userService.isLoggedIn())
+      this.router.navigateByUrl('/userprofile');
+  }
+
+  onSubmit(form: NgForm) {
+    this.userService.login(form.value).subscribe(
+      res => {
+        this.userService.setToken(res['token']);
+        this.router.navigateByUrl('/userProfile');
+      },
+      err => {
+        this.serverErrorMessages = err.error.message;
+      }
+    );
+  }
+
+}
