a02fe7c136e24ec632e351979088665f2da97e15
SangeethShiv36 <b.sangeethsivan@gmail.com>
2020-03-05 14:05:28 +0530

    added userservice to app root providers

--- client/src/app/user/sign-up/sign-up.component.ts
+++ client/src/app/user/sign-up/sign-up.component.ts
@@ -5,8 +5,7 @@ import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
-  styleUrls: ['./sign-up.component.scss'],
-  providers: [UserService]
+  styleUrls: ['./sign-up.component.scss']
 })
 export class SignUpComponent implements OnInit {
   emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
