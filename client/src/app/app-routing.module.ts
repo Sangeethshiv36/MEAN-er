e12be7a35564ba096d62193858cf0e6bc1ca97e7
SangeethShiv36 <b.sangeethsivan@gmail.com>
2020-03-05 10:06:55 +0530

    added routing

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
+import { SignUpComponent } from './user/sign-up/sign-up.component';
+import { UserComponent } from './user/user.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'signup', component: UserComponent,
+    children: [{ path: '', component: SignUpComponent }]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
